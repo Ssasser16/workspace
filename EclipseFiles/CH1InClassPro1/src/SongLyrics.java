87014d45cef991b0e071e4b5990b61d8bd2059f4
Ssasser16 <ssasser.91601@gmail.com>
2024-08-31 17:23:17 -0700

    Add files via upload

---
+++ EclipseFiles/CH1InClassPro1/src/SongLyrics.java
@@ -0,0 +1,10 @@
+// Sean Sasser 8/26/24 CH1
+public class SongLyrics {
+
+	public static void main(String[] args) {
+
+		System.out.println("Head, Shoulders Knees and Toes Knees and Toes");
+		System.out.println("Eyes and Ears and Mouth and Nose");
+	}
+
+}
---
+++ EclipseFiles/CH1InClassPro2/src/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+		System.out.println("I wanted to see what you'd do. And you didn't disappoint! You let five people die. Even to a guy like me, that's cold");
+		System.out.println("The Dark Knight");
+		System.out.println("2008");
+
+	}
+
+}
